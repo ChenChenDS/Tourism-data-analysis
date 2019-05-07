3fe6e3b232a8d019d2dba959159ee7e47fdaf4fa
ChenChen <46377474+lovelybabycc@users.noreply.github.com>
2019-05-07 02:54:12 -0400

    Add files via upload

---
+++ function.js
@@ -0,0 +1,71 @@
+function myFunction() {
+var x = document.getElementById("myDIV");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction1() {
+var x = document.getElementById("myDIV1");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction2() {
+var x = document.getElementById("myDIV2");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction3() {
+var x = document.getElementById("myDIV3");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction4() {
+var x = document.getElementById("myDIV4");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction5() {
+var x = document.getElementById("myDIV5");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction6() {
+var x = document.getElementById("myDIV6");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction7() {
+var x = document.getElementById("myDIV7");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
